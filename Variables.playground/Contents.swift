70b449d955132dc28d27e90077af6b29d9e6efa9
Jorge Calderita <jcalderita@outlook.com>
2022-11-22 17:42:01 +0100

    Variables

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+let myName = "Jorge"
+let yourName = "Foo"
+
+var names = [
+    myName,
+    yourName
+]
+names.append("Bar")
+names.append("Baz")
+
+let foo = "Foo"
+var foo2 = foo
+foo2 = "Foo 2"
+
+let moreNames = [
+    "Foo",
+    "Bar"
+]
+var copy = moreNames
+copy.append("Baz")
+moreNames
+copy
+
+let oldArray = NSMutableArray(
+    array: [
+        "Foo",
+        "Bar"
+    ]
+)
+oldArray.add("Baz")
+var newArray = oldArray
+newArray.add("Qux")
+oldArray
+newArray
+
+let someNames = NSMutableArray(
+    array: [
+        "Foo",
+        "Bar"
+    ]
+)
+func changeTheArray(_ array: NSArray) {
+    let copy = array as! NSMutableArray
+    copy.add("Baz")
+}
+changeTheArray(someNames)
+someNames
