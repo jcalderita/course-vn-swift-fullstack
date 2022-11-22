13e2263b3cd62a988977f6e206ee7a235c74a34c
Jorge Calderita <jcalderita@outlook.com>
2022-11-22 18:28:58 +0100

    Operators

---
+++ Operators.playground/Contents.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+let myAge = 22
+let yourAge = 20
+
+if myAge > yourAge {
+    "I'm older than you"
+} else if myAge < yourAge {
+    "I'm younger than you"
+} else {
+    "Oh hey, we are the same age"
+}
+
+let myMothersAge = myAge + 30
+let doubleMyAge = myAge * 2
+
+/// 1. unary prefix
+let foo = !true
+/// 2. unary postfix
+let name = Optional("Vandad")
+type(of: name)
+let unaryPostFix = name!
+type(of: unaryPostFix)
+/// 3. binary infix
+let result = 1 + 2
+let names = "Foo" + " " + "Bar"
+
+let age = 30
+let message = "You are "
++ (age >= 18
+   ? "an adult"
+   : "not yet an adult")
